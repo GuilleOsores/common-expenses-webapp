f13e699b13a02073657a5030e62cc92e13bade79
pelusa <guille.osores@gmail.com>
2019-01-19 17:50:39 -0300

    fix class in services

--- src/app/services/apartments/apartments.service.ts
+++ src/app/services/apartments/apartments.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { Apartment } from 'common-expenses-libs/libs';
 
-import { environment } from '../../../environments/environment'
+import { environment } from '../../../environments/environment';
 
-import { Apartment } from '../../classes'
 
 @Injectable({
   providedIn: 'root'
